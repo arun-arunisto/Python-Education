7104292be39f1bc3e9551f645522fd8bd6a6d01d
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-02-02 10:38:53 +0530

    Update interview_programs.py

--- interview_programs.py
+++ interview_programs.py
@@ -348,6 +348,16 @@ def minion_game(string):
         print("Kevin", int(count_k))
 
 minion_game("banana")
+"""
+1
+121
+12321
+1234321
+123454321
+"""
+n = int(input())
+for i in range(1, n+1):
+   print(((10**i-1)//9)**2)
 
 
 

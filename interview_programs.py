96e897695d0db63c874fba7cf78c751f53495aa5
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-03-19 16:33:00 +0530

    Update interview_programs.py
    Leet code roman to integer

--- interview_programs.py
+++ interview_programs.py
@@ -392,6 +392,21 @@ class Solution:
                 return False
             magazine = magazine[:matchIndex]+magazine[matchIndex+1:]
         return True
+"""
+Leet code : 13. Roman to Integer
+"""
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+        "I":1, "V":5, "X":10, "L":50,
+        "C":100, "D":500, "M":1000}
+        ans = 0
+        for i in range(0, len(s)-1):
+            if roman_dict[s[i]] < roman_dict[s[i+1]]:
+                ans -= roman_dict[s[i]]
+            else:
+                ans += roman_dict[s[i]]
+        return ans+roman_dict[s[-1]]
 
 
 

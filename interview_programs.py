b9a9d45912c4953e1ff0a85589525998dc31bf08
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2025-11-20 19:10:40 +0530

    Update interview_programs.py

--- interview_programs.py
+++ interview_programs.py
@@ -1143,3 +1143,19 @@ class Solution:
                 if magazine.count(check_str) < ransomNote.count(check_str):
                     return False
         return True
+
+"""
+Leetcode 387: First Unique Character in a String
+"""
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dictionary = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+        pos = -1
+        li = list(s)
+        for i in range(len(li)):
+            dictionary[li[i]] += 1
+        for i in range(len(li)):
+            if dictionary[li[i]] == 1:
+                pos = i
+                break
+        return pos
